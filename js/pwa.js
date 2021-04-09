f4a1a43d16c0fb83039b844c465950b5e65875a7
T-Ikehata <81730900+T-Ikehata@users.noreply.github.com>
2021-04-09 11:42:27 +0900

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-	   navigator.serviceWorker.register('/pwa-test5/js/firebase-messaging-sw.js', 
+	   navigator.serviceWorker.register('/pwa-test5/firebase-messaging-sw.js', 
 	    {scope:'/pwa-test5/'}
 	   );
 }
