2a484aaf968f8c7cb6b082afc6ef9ffbf7fbe93f
T-Ikehata <81730900+T-Ikehata@users.noreply.github.com>
2021-04-09 11:28:03 +0900

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -53,3 +53,32 @@ messaging.setBackgroundMessageHandler(function(payload) {
     notificationOptions
   );
 });
+
+// キャッシュファイルの指定
+var CACHE_NAME = 'pwa-test5-caches';
+var urlsToCache = [
+    '/pwa-test5/',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
+
